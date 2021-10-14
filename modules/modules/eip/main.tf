51d9bb36ff5e6c03267e0703a622d0e9c12b79ff
hou-zp <65334730+hou-zp@users.noreply.github.com>
2021-10-14 19:07:26 +0800

    Update main.tf

--- modules/modules/eip/main.tf
+++ modules/modules/eip/main.tf
@@ -60,29 +60,29 @@ resource "alicloud_eip_association" "this" {
   private_ip_address = lookup(local.instance_list[count.index], "private_ip_address")
 }
 
-# resource "alicloud_eip" "with_computed" {
-#   count = var.create ? var.number_of_computed_instances : 0
+resource "alicloud_eip" "with_computed" {
+  count = var.create ? var.number_of_computed_instances : 0
 
-#   name                 = var.number_of_computed_instances > 1 || var.use_num_suffix ? format("%s%03d", var.name, count.index + 1) : var.name
-#   description          = var.description
-#   bandwidth            = var.bandwidth
-#   internet_charge_type = var.internet_charge_type
-#   instance_charge_type = var.instance_charge_type
-#   period               = var.period
-#   isp                  = var.isp
-#   resource_group_id    = var.resource_group_id
-#   tags = merge(
-#     {
-#       Name = length(local.computed_instance_list) > 1 || var.use_num_suffix ? format("%s%03d", var.name, count.index + 1) : var.name
-#     },
-#     var.tags,
-#   )
-# }
-# resource "alicloud_eip_association" "with_computed" {
-#   count = var.create ? var.number_of_computed_instances : 0
+  name                 = var.number_of_computed_instances > 1 || var.use_num_suffix ? format("%s%03d", var.name, count.index + 1) : var.name
+  description          = var.description
+  bandwidth            = var.bandwidth
+  internet_charge_type = var.internet_charge_type
+  instance_charge_type = var.instance_charge_type
+  period               = var.period
+  isp                  = var.isp
+  resource_group_id    = var.resource_group_id
+  tags = merge(
+    {
+      Name = length(local.computed_instance_list) > 1 || var.use_num_suffix ? format("%s%03d", var.name, count.index + 1) : var.name
+    },
+    var.tags,
+  )
+}
+resource "alicloud_eip_association" "with_computed" {
+  count = var.create ? var.number_of_computed_instances : 0
 
-#   allocation_id      = alicloud_eip.with_computed[count.index].id
-#   instance_id        = lookup(local.computed_instance_list[count.index], "instance_id")
-#   instance_type      = lookup(local.computed_instance_list[count.index], "instance_type")
-# #   private_ip_address = lookup(local.computed_instance_list[count.index], "private_ip_address")
-# }
+  allocation_id      = alicloud_eip.with_computed[count.index].id
+  instance_id        = lookup(local.computed_instance_list[count.index], "instance_id")
+  instance_type      = lookup(local.computed_instance_list[count.index], "instance_type")
+  private_ip_address = lookup(local.computed_instance_list[count.index], "private_ip_address")
+}
